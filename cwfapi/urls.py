d961ffda7923f4ecf34e19775badc6fa8530319e
inherentspice <stephenlehanesmith@gmail.com>
2023-03-17 12:12:05 +0000

    added url route to comments

--- cwfapi/urls.py
+++ cwfapi/urls.py
@@ -5,7 +5,8 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'groups', views.GroupViewset)
 router.register(r'events', views.EventViewset)
-router.register(r'members', views.MemberViewset, basename='members')
+router.register(r'members', views.MemberViewset)
+router.register(r'comments', views.CommentViewset)
 router.register(r'users', views.UserViewset)
 router.register(r'profile', views.UserProfileViewset)
 
